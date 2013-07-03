383144938e80abff1e6cbf14c4e74b5bfb937c02
Daniel Larimer <dlarimer@gmail.com>
2013-07-03 13:26:23 -0400

    fix formatting with logging to file

--- src/log/file_appender.cpp
+++ src/log/file_appender.cpp
@@ -5,6 +5,8 @@
 #include <fc/io/fstream.hpp>
 #include <fc/variant.hpp>
 #include <fc/reflect/variant.hpp>
+#include <iomanip>
+#include <sstream>
 
 
 namespace fc {
@@ -30,15 +32,37 @@ namespace fc {
    }
    file_appender::~file_appender(){}
 
+   // MS THREAD METHOD  MESSAGE \t\t\t File:Line
    void file_appender::log( const log_message& m )
    {
-      fc::string message = fc::format_string( m.get_format(), m.get_data() );
-      fc::variant lmsg(m);
+      std::stringstream line;
+      line << (m.get_context().get_timestamp().time_since_epoch().count() % (1000ll*1000ll*60ll*60))/1000 <<"ms ";
+      line << std::setw( 10 ) << m.get_context().get_thread_name().substr(0,9).c_str() <<" ";
 
-      fc::string fmt_str = fc::format_string( my->cfg.format, mutable_variant_object(lmsg.get_object())( "message", message)  );
+      auto me = m.get_context().get_method();
+      // strip all leading scopes...
+      if( me.size() )
+      {
+         uint32_t p = 0;
+         for( uint32_t i = 0;i < me.size(); ++i )
+         {
+             if( me[i] == ':' ) p = i;
+         }
+
+         if( me[p] == ':' ) ++p;
+         line << std::setw( 20 ) << m.get_context().get_method().substr(p,20).c_str() <<" ";
+      }
+      line << "] ";
+      fc::string message = fc::format_string( m.get_format(), m.get_data() );
+      line << message.c_str();
+
+
+      //fc::variant lmsg(m);
+
+     // fc::string fmt_str = fc::format_string( my->cfg.format, mutable_variant_object(m.get_context())( "message", message)  );
       {
         fc::scoped_lock<boost::mutex> lock(my->slock);
-        my->out << fmt_str << "\n";
+        my->out << line.str() << "\t\t\t" << m.get_context().get_file() <<":"<<m.get_context().get_line_number()<<"\n";
       }
       if( my->cfg.flush ) my->out.flush();
    }

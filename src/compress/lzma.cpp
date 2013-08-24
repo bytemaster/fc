113d63c8b28457a8b24000874ed2551fe1b1acb6
Daniel Larimer <dlarimer@gmail.com>
2013-08-23 20:36:43 -0400

    various missing files

---
+++ src/compress/lzma.cpp
@@ -0,0 +1,12 @@
+#include <fc/compress/lzma.hpp>
+
+namespace fc {
+
+  std::vector<char> lzma_compress( const std::vector<char>& in )
+  {
+  }
+  std::vector<char> lzma_decompress( const std::vector<char>& compressed )
+  {
+  }
+
+} // namespace fc

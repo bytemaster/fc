5719d28a190358427836f2d492443384b2a902cc
Eric Frias <efrias@syncad.com>
2014-05-20 18:02:20 -0400

    Move/rename git revision info compiled into FC to be more self-explanatory

--- GitSHA3.h
+++
@@ -1,9 +0,0 @@
-#ifndef __GITSHA3_H
-#define __GITSHA3_H
-
-extern const char* const g_GIT_SHA3;
-extern const uint32_t g_GIT_UNIX_TIMESTAMP3;
-
-#define APPLICATION_VERSION "1.0 Beta1"
-
-#endif ///__GITSHA3_H

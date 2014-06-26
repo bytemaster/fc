456c81df27058169d4e533fda4c18e5005ac3bd2
Daniel Larimer <dlarimer@invictus-innovations.com>
2014-06-25 23:32:09 -0400

    fix potential errors in NTP caused by running in main thread

--- src/network/udt_socket.cpp
+++ src/network/udt_socket.cpp
@@ -1,6 +1,106 @@
 #include <fc/network/udt_socket.hpp>
+#include <fc/thread/thread.hpp>
+#include <fc/thread/mutex.hpp>
+#include <fc/thread/unique_lock.hpp>
+#include <fc/network/ip.hpp>
+#include <udt.h>
+
+#include <arpa/inet.h>
 
 namespace fc {
+   
+   class udt_epoll_service 
+   {
+      public:
+         udt_epoll_service()
+         :_epoll_thread("udt_epoll")
+         {
+            _epoll_id = UDT::epoll_create();
+            _epoll_loop = _epoll_thread.async( [=](){ poll_loop(); } );
+         }
+
+         ~udt_epoll_service()
+         {
+            _epoll_loop.cancel();
+         }
+
+         void poll_loop()
+         {
+            while( !_epoll_loop.canceled() )
+            {
+               std::set<UDTSOCKET> read_ready;
+               std::set<UDTSOCKET> write_ready;
+
+               UDT::epoll_wait( _epoll_id, 
+                                &read_ready, 
+                                &write_ready, 1000 );
+
+               { synchronized(_read_promises_mutex)
+                  for( auto sock : read_ready )
+                  {
+                     auto itr = _read_promises.find( sock );
+                     if( itr != _read_promises.end() )
+                     {
+                        itr->second->set_value();
+                        _read_promises.erase(itr);
+                     }
+                  }
+               } // synchronized read promise mutex
+
+               { synchronized(_write_promises_mutex)
+                  for( auto sock : write_ready )
+                  {
+                     auto itr = _write_promises.find( sock );
+                     if( itr != _write_promises.end() )
+                     {
+                        itr->second->set_value();
+                        _write_promises.erase(itr);
+                     }
+                  }
+               } // synchronized write promise mutex
+            } // while not canceled
+         } // poll_loop
+
+         void notify_read( int udt_socket_id, 
+                           const promise<void>::ptr& p )
+         {
+            int events = UDT_EPOLL_IN;
+            UDT::epoll_add_usock( _epoll_id, 
+                                  udt_socket_id, 
+                                  &events );
+
+            { synchronized(_read_promises_mutex)
+
+
+               _read_promises[udt_socket_id] = p;
+            }
+         }
+
+         void notify_write( int udt_socket_id,
+                            const promise<void>::ptr& p )
+         {
+            int events = UDT_EPOLL_OUT;
+            UDT::epoll_add_usock( _epoll_id, 
+                                  udt_socket_id, 
+                                  &events );
+
+            { synchronized(_write_promises_mutex)
+               _write_promises[udt_socket_id] = p;
+            }
+         }
+
+      private:
+         fc::mutex                                    _read_promises_mutex;
+         fc::mutex                                    _write_promises_mutex;
+         std::unordered_map<int, promise<void>::ptr > _read_promises;
+         std::unordered_map<int, promise<void>::ptr > _write_promises;
+
+         fc::future<void> _epoll_loop;
+         fc::thread _epoll_thread;
+         int        _epoll_id;
+   };
+
+
 
    void check_udt_errors()
    {
@@ -18,7 +118,7 @@ namespace fc {
    {
    }
 
-   ~udt_socket()
+   udt_socket::~udt_socket()
    {
       close();
    }
@@ -28,7 +128,7 @@ namespace fc {
       sockaddr_in serv_addr;
       serv_addr.sin_family = AF_INET;
       serv_addr.sin_port = htons(remote_endpoint.port());
-      serv_addr.sin_addr = htonl(remote_endpoint.address());
+      serv_addr.sin_addr.s_addr = htonl(remote_endpoint.get_address());
 
       // connect to the server, implict bind
       if (UDT::ERROR == UDT::connect(_udt_socket_id, (sockaddr*)&serv_addr, sizeof(serv_addr)))
@@ -40,20 +140,20 @@ namespace fc {
    { try {
       sockaddr_in peer_addr;
       int peer_addr_size = sizeof(peer_addr);
-      int error_code = UDT::getpeername( _udt_socket_id, &peer_addr, &peer_addr_size );
+      int error_code = UDT::getpeername( _udt_socket_id, (struct sockaddr*)&peer_addr, &peer_addr_size );
       if( error_code == UDT::ERROR )
           check_udt_errors();
-      return ip::endpoint( address( htonl( peer_addr.sin_addr ) ), htons(peer_addr.sin_port) );
+      return ip::endpoint( ip::address( htonl( peer_addr.sin_addr.s_addr ) ), htons(peer_addr.sin_port) );
    } FC_CAPTURE_AND_RETHROW() }
 
    ip::endpoint udt_socket::local_endpoint() const
    { try {
       sockaddr_in sock_addr;
-      int addr_size = sizeof(peer_addr);
-      int error_code = UDT::getsockname( _udt_socket_id, &sock_addr, &addr_size );
+      int addr_size = sizeof(sock_addr);
+      int error_code = UDT::getsockname( _udt_socket_id, (struct sockaddr*)&sock_addr, &addr_size );
       if( error_code == UDT::ERROR )
           check_udt_errors();
-      return ip::endpoint( address( htonl( sock_addr.sin_addr ) ), htons(sock_addr.sin_port) );
+      return ip::endpoint( ip::address( htonl( sock_addr.sin_addr.s_addr ) ), htons(sock_addr.sin_port) );
    } FC_CAPTURE_AND_RETHROW() }
 
 
@@ -63,7 +163,7 @@ namespace fc {
       auto bytes_read = UDT::recv( _udt_socket_id, buffer, max, 0 );
       if( bytes_read == UDT::ERROR )
       {
-         if( UDT::getlasterror().getCode() == UDT::EASYNCRCV )
+         if( UDT::getlasterror().getErrorCode() == CUDTException::EASYNCRCV )
          {
             // create a future and post to epoll, wait on it, then
             // call readsome recursively.
@@ -85,7 +185,7 @@ namespace fc {
    /// @{
    size_t   udt_socket::writesome( const char* buffer, size_t len )
    {
-      auto bytes_sent = UDT::send(_udt_socket_idl, buffer, len, 0);
+      auto bytes_sent = UDT::send(_udt_socket_id, buffer, len, 0);
 
       if( UDT::ERROR == bytes_sent )
          check_udt_errors();

c16bb206a39b09b03423088c831bc193de89056c
theoreticalbts <theoreticalbts@users.noreply.github.com>
2015-08-04 11:50:56 -0400

    fstream:  Implement read_file_contents

--- src/io/fstream.cpp
+++ src/io/fstream.cpp
@@ -1,7 +1,10 @@
-#include <fc/io/fstream.hpp>
-#include <fc/filesystem.hpp>
+
 #include <fstream>
+#include <sstream>
+
+#include <fc/filesystem.hpp>
 #include <fc/exception/exception.hpp>
+#include <fc/io/fstream.hpp>
 #include <fc/log/logger.hpp>
 
 #include <boost/filesystem/path.hpp>
@@ -93,5 +96,14 @@ namespace fc {
 
    bool   ifstream::eof()const { return !my->ifs.good(); }
 
+   void read_file_contents( const fc::path& filename, std::string& result )
+   {
+      const boost::filesystem::path& bfp = filename;
+      boost::filesystem::ifstream f( bfp, std::ios::in | std::ios::binary );
+      // don't use fc::stringstream here as we need something with override for << rdbuf()
+      std::stringstream ss;
+      ss << f.rdbuf();
+      result = ss.str();
+   }
   
 } // namespace fc 

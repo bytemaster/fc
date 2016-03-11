eaa763aff18e0c8c52e093ae9850a216a98ab435
Daniel Larimer <dan@bitshares.org>
2016-03-11 12:33:33 -0500

    adding canonical flag

--- include/fc/crypto/elliptic.hpp
+++ include/fc/crypto/elliptic.hpp
@@ -123,7 +123,7 @@ namespace fc {
            fc::sha512 get_shared_secret( const public_key& pub )const;
 
 //           signature         sign( const fc::sha256& digest )const;
-           compact_signature sign_compact( const fc::sha256& digest )const;
+           compact_signature sign_compact( const fc::sha256& digest, bool require_canonical = true )const;
 //           bool              verify( const fc::sha256& digest, const signature& sig );
 
            public_key get_public_key()const;
